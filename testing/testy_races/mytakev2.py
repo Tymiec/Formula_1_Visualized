371e86dd6986758cdd13f56164757b6aa3f4bea6
Tymiec <tymekbialkowski@gmail.com>
2022-04-27 02:39:35 +0200

    obejscie buga pd.read_csv, działa wykres

---
+++ testing/testy_races/mytakev2.py
@@ -0,0 +1,38 @@
+# import dash_html_components as html
+# import dash_core_components as dcc
+import dash
+import pandas as pd
+import plotly.express as px
+from dash import dcc, html, Input, Output
+from dash.dependencies import Input, Output
+# Ładujemy naszą csvke
+races = pd.read_csv("https://raw.githubusercontent.com/Tymiec/Formula_1_Visualized/master/sources/lap_times.csv")
+
+# Tworzymy dashową apke
+app = dash.Dash()
+
+# Tutaj layout apki
+app.layout = html.Div(children=[
+    html.H1(children="Czasy okrążeń"),
+    dcc.Dropdown(id="race-dropdown",
+                 options=[{"label": i, "value": i} for i in races["raceId"].unique()],
+                 value="1",
+                 ),
+    dcc.Graph(id="lap-times-graph")
+])
+
+# Set up the callback function
+
+@app.callback(
+    Output(component_id="lap-times-graph", component_property="figure"),
+    Input(component_id="race-dropdown", component_property="value")
+)
+def update_graph(selected_race):
+    filtered_races = races[races["raceId"] == selected_race]
+    line_fig = px.line(filtered_races, x="lap", y="time", color="driverId", title=f"Lap times in race number {selected_race}")
+    return line_fig
+
+# Run local server
+
+if __name__ == "__main__":
+    app.run_server(debug=True)
--- testing/testy_races/small_tests.py
+++ testing/testy_races/small_tests.py
@@ -1,2 +1,7 @@
-lap = list(range(1, 88))
-print (lap)
+# lap = list(range(1, 88))
+# print (lap)
+import pandas as pd
+
+df = pd.read_csv("https://raw.githubusercontent.com/Tymiec/Formula_1_Visualized/master/sources/lap_times.csv")
+
+print(df.to_string()) 
