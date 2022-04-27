02abe96c5d0d6d22f905d30f85b9328b7ad89d22
michalinh0 <machowski.michal01@gmail.com>
2022-04-27 16:23:14 +0200

    nowy plik źródłowy

--- testing/converter.py
+++ testing/converter.py
@@ -1,8 +1,14 @@
 import pandas as pd
 
-df = pd.read_csv("https://raw.githubusercontent.com/Tymiec/Formula_1_Visualized/master/sources/lap_times.csv")
+df = pd.read_csv("https://raw.githubusercontent.com/Tymiec/Formula_1_Visualized/master/sources/lap_times_named.csv")
 df2 = pd.read_csv("https://raw.githubusercontent.com/Tymiec/Formula_1_Visualized/master/sources/drivers.csv")
 
+for i in range(1,100):
+    df.loc[i,'driverId'] = df2.loc[df.loc[i,'driverId'],'surname']
+    print(df.loc[i,'driverId'])
+    if(i%10000 == 0):
+        print(i)
 
 
-print(df2)
+print(df)
+# df.to_csv("lap_times_named.csv", index=False)
