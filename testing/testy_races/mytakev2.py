836653c03342145321e2851a36665a4b4bbb0fcf
Tymiec <tymekbialkowski@gmail.com>
2022-04-27 02:45:24 +0200

    dodano FIXME i TODO

--- testing/testy_races/mytakev2.py
+++ testing/testy_races/mytakev2.py
@@ -5,20 +5,22 @@ import pandas as pd
 import plotly.express as px
 from dash import dcc, html, Input, Output
 from dash.dependencies import Input, Output
+
 # Ładujemy naszą csvke
 races = pd.read_csv("https://raw.githubusercontent.com/Tymiec/Formula_1_Visualized/master/sources/lap_times.csv")
+#FIXME: Z niewiadomych przyczyn pd.read_csv działa u mnie tylko dla linków
 
 # Tworzymy dashową apke
 app = dash.Dash()
 
 # Tutaj layout apki
 app.layout = html.Div(children=[
-    html.H1(children="Czasy okrążeń"),
+    html.H1(children="Czasy okrążeń"), 
     dcc.Dropdown(id="race-dropdown",
                  options=[{"label": i, "value": i} for i in races["raceId"].unique()],
                  value="1",
                  ),
-    dcc.Graph(id="lap-times-graph")
+    dcc.Graph(id="lap-times-graph")#TODO: przerobić tak aby zajmował 50% wysokości strony
 ])
 
 # Set up the callback function
@@ -31,6 +33,7 @@ def update_graph(selected_race):
     filtered_races = races[races["raceId"] == selected_race]
     line_fig = px.line(filtered_races, x="lap", y="time", color="driverId", title=f"Lap times in race number {selected_race}")
     return line_fig
+#FIXME: z niewiadomych przyczyn wykres jest nieczytelny, a w sumie to z wiadomych, trzeba po prostu go zwiększyć na całą stronę i będzie dobrze
 
 # Run local server
 
