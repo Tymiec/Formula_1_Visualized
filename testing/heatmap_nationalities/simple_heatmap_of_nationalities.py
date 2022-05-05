b97ffee80538e657e0c0ff2b7f640148c1bfd6b7
Tymiec <tymekbialkowski@gmail.com>
2022-05-05 03:50:30 +0200

    name unification

--- testing/heatmap_nationalities/simple_heatmap_of_nationalities.py
+++ testing/heatmap_nationalities/simple_heatmap_of_nationalities.py
@@ -5,5 +5,5 @@ df = pd.read_csv(r"D:\Repo\Wizualizacja_projekt\testing\heatmap_narodowosci\nati
 fig = px.choropleth(df, locations="country_code",
                     color="value", # lifeExp is a column of gapminder
                     hover_name="country_code", # column to add to hover information
-                    color_continuous_scale=px.colors.sequential.Plasma)
+                    color_continuous_scale=px.colors.sequential.Reds)
 fig.show()
