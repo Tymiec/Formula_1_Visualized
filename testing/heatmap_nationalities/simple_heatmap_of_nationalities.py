c667f8f165422e210dafca65af63293800e83b5a
Tymiec <tymekbialkowski@gmail.com>
2022-05-05 17:22:47 +0200

    zmiana koloru i trybu wyświetlania na 3D

--- testing/heatmap_nationalities/simple_heatmap_of_nationalities.py
+++ testing/heatmap_nationalities/simple_heatmap_of_nationalities.py
@@ -2,8 +2,15 @@ import plotly.express as px
 import pandas as pd
 
 df = pd.read_csv("https://raw.githubusercontent.com/Tymiec/Formula_1_Visualized/master/testing/heatmap_nationalities/nationalities_counted.csv")
+# fig = px.choropleth(df, locations="country_code",
+#                     color="value", # lifeExp is a column of gapminder
+#                     hover_name="country_code", # column to add to hover information
+#                     color_continuous_scale=px.colors.sequential.YlOrRd)
+
 fig = px.choropleth(df, locations="country_code",
                     color="value", # lifeExp is a column of gapminder
                     hover_name="country_code", # column to add to hover information
-                    color_continuous_scale=px.colors.sequential.YlOrRd)
+                    color_continuous_scale=px.colors.sequential.YlOrRd,
+                    projection="orthographic")
+
 fig.show()
