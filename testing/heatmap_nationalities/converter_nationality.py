b90dd63b523d526c0992be3b6e9a1b45cdb899eb
Tymiec <tymekbialkowski@gmail.com>
2022-05-05 03:58:59 +0200

    zmiana koloru + ściezki -> linki

--- testing/heatmap_nationalities/converter_nationality.py
+++ testing/heatmap_nationalities/converter_nationality.py
@@ -1,6 +1,6 @@
 import io
 import pandas as pd
-df = pd.read_csv(r"D:\Repo\Wizualizacja_projekt\testing\heatmap_narodowosci\drivers_country_code.csv")
+df = pd.read_csv("https://raw.githubusercontent.com/Tymiec/Formula_1_Visualized/master/testing/heatmap_nationalities/drivers_country_code.csv")
 df2 = df[["country_code"]]
 df2["occurences"] = 1 # można wpisać cokolwiek bo liczy i tak tylko wystąpienia
 df2 = df2.groupby(["country_code"])["occurences"].count()
--- testing/heatmap_nationalities/simple_heatmap_of_nationalities.py
+++ testing/heatmap_nationalities/simple_heatmap_of_nationalities.py
@@ -1,9 +1,9 @@
 import plotly.express as px
 import pandas as pd
 
-df = pd.read_csv(r"D:\Repo\Wizualizacja_projekt\testing\heatmap_narodowosci\nationalities_counted.csv")
+df = pd.read_csv("https://raw.githubusercontent.com/Tymiec/Formula_1_Visualized/master/testing/heatmap_nationalities/nationalities_counted.csv")
 fig = px.choropleth(df, locations="country_code",
                     color="value", # lifeExp is a column of gapminder
                     hover_name="country_code", # column to add to hover information
-                    color_continuous_scale=px.colors.sequential.Reds)
+                    color_continuous_scale=px.colors.sequential.YlOrRd)
 fig.show()
