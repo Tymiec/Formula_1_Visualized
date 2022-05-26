943f078c010aedb94d48b028d47a7943a7435a83
WisniaN7 <WisniaN7@gmail.com>
2022-05-26 11:23:00 +0200

    Rozdzielono czasy okrążeń wyścigów na osobne pliki

---
+++ tools/split races.py
@@ -0,0 +1,24 @@
+#%%
+import pandas as pd
+
+df = pd.read_csv('https://raw.githubusercontent.com/Tymiec/Formula_1_Visualized/master/sources/lap_tunes_named_3.csv')
+
+#%%
+years = []
+
+for year in df.year.unique():
+    years.append(df[ df['year'] == year ])
+
+#%%
+GPs = []
+
+for year_df in years:
+    for GP in year_df.name_x.unique():
+        GPs.append(year_df[ year_df['name_x'] == GP ])
+
+#%%
+for GPs_df in GPs:
+    GPs_df_export = GPs_df.drop(columns=['year', 'name_x', 'raceId'])
+    GPs_df_export.to_csv('GPs/' + str(GPs_df.year.unique()[0]) + ' ' + str(GPs_df.name_x.unique()[0]) + '.csv', index=False)
+
+# %%
