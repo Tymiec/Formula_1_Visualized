b7200ca76431cd9b0b5c4f987a7287e47ed42984
michalinh0 <machowski.michal01@gmail.com>
2022-05-31 13:10:23 +0200

    Pierwsza wersja wykresu ze zwycięzcami wyścigów + aktualizacja csv

---
+++ testing/Winners.py
@@ -0,0 +1,49 @@
+import dash
+import pandas as pd
+import plotly.express as px
+from dash import dcc, html, Input, Output
+from dash.dependencies import Input, Output
+
+# Ładujemy naszą csvke
+races = pd.read_csv("https://raw.githubusercontent.com/Tymiec/Formula_1_Visualized/master/sources/winners.csv")
+#races = races.sort_values(by="lap") # Sortujemy dla pewności
+
+
+# Tworzymy dashową apke
+app = dash.Dash()
+
+# Tutaj layout apki
+app.layout = html.Div(children=[
+    html.H1(children="F1 Visualized"), 
+    dcc.RadioItems(
+                ['drivers', 'constructors'],
+                'drivers',
+                id='graph_type',
+                inline=True
+                ),
+    dcc.Graph(id="driver_standings")
+], style={"background-color" : "#ff0"})
+
+# Set up the callback function
+
+@app.callback(
+    Output(component_id="driver_standings", component_property="figure"),
+    Input(component_id='graph_type' , component_property='value')
+)
+def update_graph(graph_type):
+    print(graph_type)
+    if(graph_type == 'drivers'):
+        winners = races['driverId'].value_counts()
+        print(winners)
+        fig = px.bar(winners)
+        return fig
+    if(graph_type == 'constructors'):
+        winners = races['constructorId'].value_counts()
+        print(winners)
+        fig = px.bar(winners)
+        return fig
+
+# Run local server
+
+if __name__ == "__main__":
+    app.run_server(debug=True)
