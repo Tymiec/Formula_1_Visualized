497e91ea0c0a73443b350920ff399f89a1dce3a6
michalinh0 <machowski.michal01@gmail.com>
2022-06-06 16:07:43 +0200

    Naprawa animacji ( nie ruszaj już )

---
+++ final/presentation/animacja_kierowcow copy.py
@@ -0,0 +1,24 @@
+import plotly.express as px
+import pandas as pd
+
+df = pd.read_csv("https://raw.githubusercontent.com/Tymiec/Formula_1_Visualized/master/sources/modified/nationalities_with_country_no_zeroes.csv")
+df = df.sort_values(by="Year")
+# print(df)
+# fig = px.choropleth(df, locations="country_code",
+#                     color="value", # lifeExp is a column of gapminder
+#                     hover_name="country_code", # column to add to hover information
+#                     color_continuous_scale=px.colors.sequential.YlOrRd)
+
+
+print(df)
+
+fig = px.choropleth(df, locations="Country",
+                    locationmode = 'country names',
+                    animation_frame="Year",
+                    color="Kolumna2", # lifeExp is a column of gapminder
+                    hover_name="Country", # column to add to hover information
+                    color_continuous_scale=px.colors.sequential.YlOrRd,
+                    # projection="orthographic"
+                    )
+
+fig.show()
