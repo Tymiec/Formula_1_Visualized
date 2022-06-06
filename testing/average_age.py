0cfcfe13f8d3e73d6683a9865048e44c62a74d52
michalinh0 <machowski.michal01@gmail.com>
2022-06-06 17:58:07 +0200

    Średni wiek

--- testing/average_age.py
+++ testing/average_age.py
@@ -3,7 +3,7 @@ import pandas as pd
 df = pd.read_csv('https://raw.githubusercontent.com/Tymiec/Formula_1_Visualized/master/sources/modified/results_modified_with_ages.csv')
 df = df.sort_values(by="raceId")
 
-df2 = df[["raceId","year","Age","position"]]
-df2 = df2.filter(like="1" , axis = 0)
+df2 = df[["raceId","year","Age"]]
 print(df2)
-df2=df.groupby(["raceId","year"])["Age"].mean()
+df2=df.groupby(["year"])["Age"].mean()
+print(df2)
