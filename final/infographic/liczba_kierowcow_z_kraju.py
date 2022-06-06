f4af767455cfdaf924d67f15713abb1455495d26
Tymiec <tymekbialkowski@gmail.com>
2022-06-06 15:45:41 +0200

    fixed

--- final/infographic/liczba_kierowcow_z_kraju.py
+++ final/infographic/liczba_kierowcow_z_kraju.py
@@ -3,16 +3,11 @@ import pandas as pd
 
 df = pd.read_csv("https://raw.githubusercontent.com/Tymiec/Formula_1_Visualized/master/sources/modified/nationalities_with_country_no_zeroes.csv")
 df = df.sort_values(by="Year")
-# print(df)
-# fig = px.choropleth(df, locations="country_code",
-#                     color="value", # lifeExp is a column of gapminder
-#                     hover_name="country_code", # column to add to hover information
-#                     color_continuous_scale=px.colors.sequential.YlOrRd)
 
 fig = px.choropleth(df, locations="Country",
                     locationmode = 'country names',
                     animation_frame="Year",
-                    color="Liczba_Kierowców", # lifeExp is a column of gapminder
+                    color="Liczba_Kierowcow", # lifeExp is a column of gapminder
                     hover_name="Country", # column to add to hover information
                     color_continuous_scale=px.colors.sequential.YlOrRd,
                     # projection="orthographic"
