aea28f00597775352921e254de9acdf84a8520dc
Tymiec <tymekbialkowski@gmail.com>
2022-06-06 15:47:18 +0200

    working

--- final/infographic/liczba_kierowcow_z_kraju.py
+++ final/infographic/liczba_kierowcow_z_kraju.py
@@ -1,7 +1,7 @@
 import plotly.express as px
 import pandas as pd
 
-df = pd.read_csv("https://raw.githubusercontent.com/Tymiec/Formula_1_Visualized/master/sources/modified/nationalities_with_country_no_zeroes.csv")
+df = pd.read_csv("https://raw.githubusercontent.com/Tymiec/Formula_1_Visualized/master/sources/modified/nationalities_with_country_no_zeroes_copy.csv")
 df = df.sort_values(by="Year")
 
 fig = px.choropleth(df, locations="Country",
