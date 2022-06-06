17681bea8afdaa9ad01d3c4445c185f41955cbe0
michalinh0 <machowski.michal01@gmail.com>
2022-06-06 12:04:00 +0200

    Animacja ilości kierowców

--- tools/animation.py
+++ tools/animation.py
@@ -2,14 +2,15 @@ import plotly.express as px
 import pandas as pd
 
 df = pd.read_csv("https://raw.githubusercontent.com/Tymiec/Formula_1_Visualized/master/sources/modified/nationalities_with_country.csv")
-df["countrycode"]
+df = df.sort_values(by="Year")
 # fig = px.choropleth(df, locations="country_code",
 #                     color="value", # lifeExp is a column of gapminder
 #                     hover_name="country_code", # column to add to hover information
 #                     color_continuous_scale=px.colors.sequential.YlOrRd)
 
 fig = px.choropleth(df, locations="Country",
-                    # animation_frame="year",
+                    locationmode = 'country names',
+                    animation_frame="Year",
                     color="Kolumna2", # lifeExp is a column of gapminder
                     hover_name="Country", # column to add to hover information
                     color_continuous_scale=px.colors.sequential.YlOrRd,
