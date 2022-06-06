4ee9b56d1a68460aeba074ee6074792e25596d6b
Tymiec <79647429+Tymiec@users.noreply.github.com>
2022-06-06 17:03:42 +0200

    dodanie piecharta kierowców

---
+++ final/infographic/kierowcy_piechart.py
@@ -0,0 +1,14 @@
+import plotly.express as px
+import pandas as pd
+
+df = pd.read_csv("https://raw.githubusercontent.com/Tymiec/Formula_1_Visualized/master/testing/heatmap_nationalities/nationalities_counted.csv")
+
+fig = px.pie(df,
+             values="value", 
+             names="country_code", 
+             hole=.3,
+             title="Narodowości Formuły 1",
+             labels="country_code")
+fig.update_traces(textposition='inside', textinfo='percent+label')
+
+fig.show()
