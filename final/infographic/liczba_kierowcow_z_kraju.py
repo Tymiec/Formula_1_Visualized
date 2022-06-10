0f6099fbce1547882024e7c05dd55f433e0954c6
michalinh0 <machowski.michal01@gmail.com>
2022-06-10 10:28:06 +0200

    Kosmetyczne zmiany

--- final/infographic/liczba_kierowcow_z_kraju.py
+++ final/infographic/liczba_kierowcow_z_kraju.py
@@ -1,7 +1,7 @@
 import plotly.express as px
 import pandas as pd
 
-df = pd.read_csv("https://raw.githubusercontent.com/Tymiec/Formula_1_Visualized/master/sources/modified/nationalities_with_country.csv")
+df = pd.read_csv("https://raw.githubusercontent.com/Tymiec/Formula_1_Visualized/master/sources/modified/nationalities_with_country_no_zeroes.csv")
 df = df.sort_values(by="Year")
 # print(df)
 # fig = px.choropleth(df, locations="country_code",
@@ -12,7 +12,7 @@ df = df.sort_values(by="Year")
 fig = px.choropleth(df, locations="Country",
                     locationmode = 'country names',
                     animation_frame="Year",
-                    color="Kolumna2", # lifeExp is a column of gapminder
+                    color="Liczba_kierowcow", # lifeExp is a column of gapminder
                     hover_name="Country", # column to add to hover information
                     color_continuous_scale=px.colors.sequential.YlOrRd,
                     # projection="orthographic"
