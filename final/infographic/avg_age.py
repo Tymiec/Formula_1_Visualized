725397b1a559ea9275c99d40eb5d556073d494b2
WisniaN7 <WisniaN7@gmail.com>
2022-06-10 15:46:35 +0200

    Dodano najstarszego i najmłodszego kierowcę w każdym roku

--- final/infographic/avg_age.py
+++ final/infographic/avg_age.py
@@ -3,5 +3,5 @@ import plotly.express as px
 
 df = pd.read_csv("https://raw.githubusercontent.com/Tymiec/Formula_1_Visualized/master/sources/modified/avgage.csv")
 
-fig = px.line(df, x="year", y="Age", line_shape="spline", render_mode="svg")
+fig = px.line(df, x="year", y="val", color='type', line_shape="spline", render_mode="svg")
 fig.show()
